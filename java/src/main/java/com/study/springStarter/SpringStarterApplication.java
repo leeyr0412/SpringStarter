3ece90bd74470a8f48c4ea9a4f5bb1d59f313dc8
yeri <leeyr0412@naver.com>
2024-06-13 18:58:29 +0900

    :sparkles: feat: JPA, env 설정

--- java/src/main/java/com/study/springStarter/SpringStarterApplication.java
+++ java/src/main/java/com/study/springStarter/SpringStarterApplication.java
@@ -1,13 +1,20 @@
 package com.study.springStarter;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringStarterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringStarterApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        SpringApplication.run(SpringStarterApplication.class, args);
+    }
 
 }
