e6b0b4e54a6a635d0cb51efdfac0a55a4de63c50
yeri <leeyr0412@naver.com>
2024-06-20 23:28:56 +0900

    :sparkles: feat: 잘못된 입력 예외처리

---
+++ java/src/main/java/com/study/springStarter/common/exception/InvalidInputException.java
@@ -0,0 +1,15 @@
+package com.study.springStarter.common.exception;
+
+import lombok.Getter;
+
+@Getter
+public class InvalidInputException extends RuntimeException {
+    private final String fieldName;
+    private final String message;
+
+    public InvalidInputException(String fieldName, String message) {
+        super(message);
+        this.fieldName = fieldName;
+        this.message = message;
+    }
+}
