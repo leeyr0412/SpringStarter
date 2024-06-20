4bce0182728c56ee565826579d2db2e4e03ca790
yeri <leeyr0412@naver.com>
2024-06-20 22:45:55 +0900

    :sparkles: feat: 회원가입 Request valid 적용

--- java/src/main/java/com/study/springStarter/member/controller/request/MemberRegisterRequest.java
+++ java/src/main/java/com/study/springStarter/member/controller/request/MemberRegisterRequest.java
@@ -6,10 +6,23 @@ import java.time.LocalDate;
 
 @Data
 public class MemberRegisterRequest {
+
+    @NotBlank
+    @Pattern(regexp = "^[a-zA-Z0-9_]{5,20}",
+            message = "영문, 숫자, _만을 사용하세요")
     private String loginId;
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@$%^&*])[a-zA-Z0-9!@$%^&*]{8,20}",
+            message = "영문, 숫자, 특수문자를 포함한 10~20자리로 입력해주세요")
     private String password;
+    @NotBlank
+    @Email(message = "이메일 형식을 확인하세요")
     private String email;
+    @NotBlank
+    @Pattern(regexp = "^[ㄱ-힣]{1,10}",
+            message = "올바른 이름을 입력하세요")
     private String name;
+    @Past(message = "유효한 날짜를 입력하세요")
     private LocalDate birthDate;
 }
 
