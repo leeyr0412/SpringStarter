eedc4497cc6f53d4ca45b6f59b8d4480060540ea
yeri <leeyr0412@naver.com>
2024-06-20 23:55:09 +0900

    :sparkles: refactor: 회원가입 Controller 수정

--- java/src/main/java/com/study/springStarter/member/controller/MemberController.java
+++ java/src/main/java/com/study/springStarter/member/controller/MemberController.java
@@ -1,7 +1,9 @@
 package com.study.springStarter.member.controller;
 
+import com.study.springStarter.common.response.BaseResponse;
 import com.study.springStarter.member.controller.request.MemberRegisterRequest;
 import com.study.springStarter.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +18,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
-    public String signup(@RequestBody MemberRegisterRequest request) {
-        return memberService.signUp(request);
+    public BaseResponse<?> signup(@RequestBody @Valid MemberRegisterRequest request) {
+        return BaseResponse.ok(memberService.signUp(request));
     }
 }
