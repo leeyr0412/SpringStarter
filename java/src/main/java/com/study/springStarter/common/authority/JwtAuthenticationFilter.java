88ed87ef11e9e40bb94d14cff69fa9d4939fd394
yeri <leeyr0412@naver.com>
2024-06-28 18:58:05 +0900

    :sparkles: feat: JwtAuthenticationFilter 구현

---
+++ java/src/main/java/com/study/springStarter/common/authority/JwtAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package com.study.springStarter.common.authority;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.study.springStarter.common.response.BaseResponse;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends GenericFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        try {
+            String token = resolveToken((HttpServletRequest) request);
+
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            chain.doFilter(request, response);
+        } catch (JwtException | IllegalArgumentException e) {
+            HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+            httpServletResponse.setContentType("application/json;charset=UTF-8");
+
+            Map<String, String> errors = new HashMap<>();
+            errors.put("token", e.getMessage());
+
+            BaseResponse<?> errorResponse = BaseResponse.error(HttpStatus.UNAUTHORIZED, errors);
+
+            httpServletResponse.getWriter().write(new ObjectMapper().writeValueAsString(errorResponse));
+        }
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
