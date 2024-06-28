3173a21e1934ad54442ee4cd0dcabe4b03e5a501
yeri <leeyr0412@naver.com>
2024-06-28 19:11:32 +0900

    :sparkles: feat: 로그인 서비스 구현

--- java/src/main/java/com/study/springStarter/member/service/MemberService.java
+++ java/src/main/java/com/study/springStarter/member/service/MemberService.java
@@ -1,22 +1,22 @@
 package com.study.springStarter.member.service;
 
+import com.study.springStarter.common.authority.JwtTokenProvider;
+import com.study.springStarter.common.authority.TokenInfo;
 import com.study.springStarter.common.exception.InvalidInputException;
 import com.study.springStarter.common.status.Role;
-//import com.study.springStarter.member.controller.request.LoginRequest;
+import com.study.springStarter.member.controller.request.LoginRequest;
 import com.study.springStarter.member.controller.request.MemberRegisterRequest;
 import com.study.springStarter.member.entity.Member;
 import com.study.springStarter.member.entity.MemberRole;
 import com.study.springStarter.member.repository.MemberRepository;
 import com.study.springStarter.member.repository.MemberRoleRepository;
 import lombok.RequiredArgsConstructor;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.core.Authentication;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
 @Transactional
@@ -24,8 +24,8 @@ import java.util.Optional;
 public class MemberService {
     private final MemberRepository memberRepository;
     private final MemberRoleRepository memberRoleRepository;
-    //private final AuthenticationManagerBuilder authenticationManagerBuilder;
-    //private final JwtTokenProvider jwtTokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final JwtTokenProvider jwtTokenProvider;
     private final BCryptPasswordEncoder passwordEncoder;
 
     /**
@@ -46,5 +46,15 @@ public class MemberService {
         memberRoleRepository.save(memberRole);
 
         return "가입되었습니다.";
+    }
+
+    /**
+     * 로그인
+     */
+    public TokenInfo login(LoginRequest request) {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(request.getLoginId(), request.getPassword());
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        return jwtTokenProvider.createToken(authentication);
     }
 }
