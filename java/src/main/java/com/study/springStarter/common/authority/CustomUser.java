041205b451a2d374d020fd993a26182832f3bc4e
yeri <leeyr0412@naver.com>
2024-06-28 17:55:54 +0900

    :sparkles: feat: CustomUser 작성

---
+++ java/src/main/java/com/study/springStarter/common/authority/CustomUser.java
@@ -0,0 +1,22 @@
+package com.study.springStarter.common.authority;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class CustomUser extends User {
+    private Long userId;
+    private String userName;
+    private String password;
+    private Collection<GrantedAuthority> authorities;
+
+    public CustomUser(Long userId, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+        this.userId = userId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+}
