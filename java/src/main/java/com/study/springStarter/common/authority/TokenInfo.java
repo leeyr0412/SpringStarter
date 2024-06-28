ef8ceacec5ba22425f9219029f3c225af0413157
yeri <leeyr0412@naver.com>
2024-06-28 15:58:39 +0900

    :sparkles: feat: tokenInfo 작성

---
+++ java/src/main/java/com/study/springStarter/common/authority/TokenInfo.java
@@ -0,0 +1,14 @@
+package com.study.springStarter.common.authority;
+
+import lombok.Data;
+
+@Data
+public class TokenInfo {
+    private String grantType;
+    private String accessToken;
+
+    public TokenInfo(String grantType, String accessToken) {
+        this.grantType = grantType;
+        this.accessToken = accessToken;
+    }
+}
